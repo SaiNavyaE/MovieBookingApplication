2efb1d8f6e9701772e662d4cdbab1ce0d4cea431
navya <sainavya.edla@gmail.com>
2022-09-13 23:33:16 +0530

    Checkpoint 2

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Header from './common/header/Header';
+import Home from './screens/home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
 
