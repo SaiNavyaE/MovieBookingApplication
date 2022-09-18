0e9273ad551c3f5cc8c09e42696fc96769aba714
navya <sainavya.edla@gmail.com>
2022-09-18 23:51:00 +0530

    movie booking application

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -3,9 +3,14 @@ import './Header.css';
 class Header extends Component{
   render(){
     return(
+    <div>
       <div className="header-div">
         <img className="svg-image" src="https://cdn.upgrad.com/uploads/production/286e1f11-1897-4d0c-ab0f-6b2bfc1ce642/logo.svg"/>
       </div>
+      <div className="upcoming-mvs-header">
+        Upcoming Movies
+      </div>
+    </div>
     )
   }
 }
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,11 +1,18 @@
 import React, {Component} from "react";
 import Header from "../../common/header/Header";
 import './Home.css';
+
+//import GridList from '@material-ui/core/GridList';
+//import GridListTile from '@material-ui/core/GridListTile';
+//import GridListTileBar from '@material-ui/core/GridListTileBar';
+
 class Home extends Component{
   render(){
     return(
       <div >
         <Header />
+
+        <script src="./common/moviesData.js"></script>
       </div>
     )
   }
